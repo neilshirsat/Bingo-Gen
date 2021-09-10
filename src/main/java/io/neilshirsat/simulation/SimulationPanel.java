67cd2a36c69fea61132ae5a28235bb347ec32812
neilshirsat <neilshirsat2015@gmail.com>
2021-09-10 06:57:10 -0500

    Create Simulation

--- src/main/java/io/neilshirsat/simulation/SimulationPanel.java
+++ src/main/java/io/neilshirsat/simulation/SimulationPanel.java
@@ -1,4 +1,13 @@
 package io.neilshirsat.simulation;
 
+import io.neilshirsat.components.UtilityWindow;
+
+import javax.swing.*;
+
 public class SimulationPanel {
+
+    private UtilityWindow Window;
+
+
+
 }
